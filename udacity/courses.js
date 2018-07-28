9608c85fb7b620bc4e1235bb16fb4a3cf6e50bd9
motatoes <moe.habib9@gmail.com>
2018-07-28 16:21:27 +0100

    cal

--- udacity/courses.js
+++ udacity/courses.js
@@ -64,5 +64,80 @@ var courses = {
         })
         console.log(coursesToSchedule)
     })
+
+    // gcal("AIzaSyATTSdIfRvu9ilGXCDyyVVbvKmyhHPOA7g")v.freebusy.query({
+    window.gapi.client.calendar.freebusy.query({
+       "items":[{
+           "id": "primary"
+       }],
+       "timeMin": startDate.toISOString(),
+       "timeMax": endDate.toISOString(),
+    }).then(function(data) {
+      console.log(data)
+       // if(err) return console.log(err)
+
+       // then calculate free slots
+       var slots = slotsFromEvents(startDate, data.result.calendars["primary"].busy)
+       console.log(slots)
+    })
+
+
+
   }
+}
+
+
+
+var now = new Date();
+var startDate = new Date(),
+   endDate = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+
+var rootStart = startDate,
+   rootEnd = endDate;
+
+
+var interval = 0.5, // 30 minutes slots
+freeSlots = [];
+
+function slotsFromEvents(date,events) {
+   events.forEach(function (event, index) { //calculate free from busy times
+       if (index == 0 && startDate < event.start) {
+           freeSlots.push({startDate: startDate, endDate: event.start});
+       }
+       else if (index == 0) {
+           startDate = event.end;
+       }
+       else if (events[index - 1].end < event.start) {
+           freeSlots.push({startDate: events[index - 1].end, endDate: event.start});
+       }
+
+       if (events.length == (index + 1) && event.end < endDate) {
+           freeSlots.push({startDate: event.end, endDate: endDate});
+       }
+   });
+
+
+   if (events.length == 0) {
+       freeSlots.push({startDate: startDate, endDate: endDate});
+   }
+
+   var temp = {}, hourSlots = [];
+   freeSlots.forEach(function(free, index) {
+       var freeHours = new Date(free.endDate).getHours() - new Date(free.startDate).getHours(), freeStart = new Date(free.startDate), freeEnd = new Date(free.endDate);
+       while(freeStart.getHours()+freeHours+interval>=0) { // 11 + 4 + 2 >= 0
+           if(freeHours>=interval) {
+               temp.e = new Date(free.startDate);
+               temp.e.setHours(temp.e.getHours()+freeHours);
+               temp.s = new Date(free.startDate);
+               temp.s.setHours(temp.s.getHours()+freeHours-interval);
+               if(temp.s.getHours() >= rootStart.getHours() && temp.e.getHours() <= rootEnd.getHours()) {
+                   hourSlots.push({calName: "calendar", startDate:temp.s, endDate:temp.e});
+                   temp = {};
+               }
+           }
+           freeHours--;
+       }
+   })
+   return [freeSlots, hourSlots]
+   // callBack(freeSlots, hourSlots);
 }
